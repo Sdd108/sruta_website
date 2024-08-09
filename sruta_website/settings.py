f216d6bdd193cc3f66c16c6d7d6bade2a5d4a2b2
“Zhipeng <zhipengmail@qq.com>
2024-08-09 21:18:49 +0800

    add some conf

--- sruta_website/settings.py
+++ sruta_website/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os.path
+
 import pymysql
 from pathlib import Path
 
@@ -120,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
