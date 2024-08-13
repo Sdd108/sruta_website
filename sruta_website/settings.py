ff84013156c7de864537f62b475ef2d2d5a8a486
“Zhipeng <zhipengmail@qq.com>
2024-08-13 22:30:56 +0800

    update config for deployment

--- sruta_website/settings.py
+++ sruta_website/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--j*$nc4c$0driy4z+8^fzb4h#hy0t3@q*6ma&o*uglw5t^17qt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
